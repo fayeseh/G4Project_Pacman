3f5d2efdcc7b08e2efcd70c7bf46382dd03df594
YanlinPang <greenearth1541@gmail.com>
2021-06-19 17:58:37 +0800

    The class 'InputDataPreperation' added.'

---
+++ Pacman AI/include/InputDataPreperation.h
@@ -0,0 +1,16 @@
+#ifndef INPUTDATAPREPERATION_H
+#define INPUTDATAPREPERATION_H
+
+
+class InputDataPreperation
+{
+    public:
+        InputDataPreperation();
+        virtual ~InputDataPreperation();
+
+    protected:
+
+    private:
+};
+
+#endif // INPUTDATAPREPERATION_H
